6536b434d28a0346912fa4514af0097b90c5f4b8
HmamSr <humamnuqi2@gmail.com>
2026-07-16 08:25:58 +0700

    fix: ssl constant for php 8.5 and view compiled path

--- config/database.php
+++ config/database.php
@@ -61,7 +61,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => !env('DB_SSLMODE'),
+            ], fn($v) => $v !== null && $v !== false) : [],
         ],
 
         'mariadb' => [
