0e6a8ad7c743ab2c8c357097f6a4e634b6307464
HmamSr <humamnuqi2@gmail.com>
2026-07-16 08:45:12 +0700

    fix: serve static files via api/index.php

--- api/index.php
+++ api/index.php
@@ -20,6 +20,53 @@ if ($isVercel) {
 
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+    $staticMappings = [
+        '/build/' => '/public/build/',
+        '/images/' => '/public/images/',
+    ];
+
+    foreach ($staticMappings as $prefix => $dir) {
+        if (str_starts_with($uri, $prefix)) {
+            $relativePath = substr($uri, strlen($prefix) - 1);
+            $filePath = realpath(__DIR__ . '/..' . $dir . $relativePath);
+            $realDir = realpath(__DIR__ . '/..' . $dir);
+            if ($filePath && $realDir && str_starts_with($filePath, $realDir) && is_file($filePath)) {
+                $ext = strtolower(pathinfo($filePath, PATHINFO_EXTENSION));
+                $mimeTypes = [
+                    'css' => 'text/css',
+                    'js' => 'application/javascript',
+                    'json' => 'application/json',
+                    'png' => 'image/png',
+                    'jpg' => 'image/jpeg',
+                    'jpeg' => 'image/jpeg',
+                    'gif' => 'image/gif',
+                    'svg' => 'image/svg+xml',
+                    'ico' => 'image/x-icon',
+                    'woff' => 'font/woff',
+                    'woff2' => 'font/woff2',
+                    'txt' => 'text/plain',
+                    'pdf' => 'application/pdf',
+                ];
+                header('Content-Type: ' . ($mimeTypes[$ext] ?? 'application/octet-stream'));
+                header('Content-Length: ' . filesize($filePath));
+                header('Cache-Control: public, max-age=31536000, immutable');
+                readfile($filePath);
+                exit;
+            }
+        }
+    }
+
+    $staticRootFiles = ['/favicon.ico' => '/public/favicon.ico', '/robots.txt' => '/public/robots.txt'];
+    if (isset($staticRootFiles[$uri])) {
+        $filePath = realpath(__DIR__ . '/..' . $staticRootFiles[$uri]);
+        if ($filePath && is_file($filePath)) {
+            header('Content-Type: mime_content_type($filePath)');
+            header('Content-Length: ' . filesize($filePath));
+            readfile($filePath);
+            exit;
+        }
+    }
+
     if (str_starts_with($uri, '/storage/')) {
         $relativePath = substr($uri, strlen('/storage/'));
         $storageRoot = '/tmp/storage/app/public';
